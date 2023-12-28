ac2ae660121edf170b0e8189fea343e9831da066
TimurNiki <timurniki95@gmail.com>
2023-12-28 22:31:49 +0400

    splash v1

--- lib/feature/pages/splash/splash_screen.dart
+++ lib/feature/pages/splash/splash_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:firebase_test_app/feature/widgets/animation/wavy_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -16,7 +15,6 @@ class SplashScreen extends ConsumerWidget {
           children: [
             Image(image: AssetImage(Images.randomLogo)),
             WavyText()
-              
           ],
         ),
       ),
